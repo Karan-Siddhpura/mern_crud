b09556f326363f41b579749627b5ae15334bf974
Your Name <siddhpura306@gmail.com>
2023-06-19 10:27:52 +0530

    Add Base URL 1

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/userControl");
 require("dotenv").config();
-const baseUrl = process.env.BASE_URL
-
-router.get(`/${baseUrl}/get"`, userCtrl.getUser);
+const baseUrl = "https://mern-crud-nn6k.onrender.com"
+router.get(`/${baseUrl}/get`, userCtrl.getUser);
 router.post(`/${baseUrl}/save`, userCtrl.saveUser);
 router.put(`/${baseUrl}/update/:id`, userCtrl.updateUser);
 router.delete(`/${baseUrl}/delete/:id`, userCtrl.deleteUser);

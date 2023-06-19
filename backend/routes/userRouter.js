a3bd959ec6b2e7e019bf23e65ce0696dda58e8f3
Your Name <siddhpura306@gmail.com>
2023-06-19 10:19:35 +0530

    Add Base URL

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/userControl");
+require("dotenv").config();
+const baseUrl = process.env.BASE_URL
 
-router.get("/get", userCtrl.getUser);
-router.post("/save", userCtrl.saveUser);
-router.put("/update/:id", userCtrl.updateUser);
-router.delete("/delete/:id", userCtrl.deleteUser);
+router.get(`/${baseUrl}/get"`, userCtrl.getUser);
+router.post(`/${baseUrl}/save`, userCtrl.saveUser);
+router.put(`/${baseUrl}/update/:id`, userCtrl.updateUser);
+router.delete(`/${baseUrl}/delete/:id`, userCtrl.deleteUser);
 
 module.exports = router;

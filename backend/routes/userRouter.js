f30aa1920eb5e0c35ef3fd3749c1191e84fd9e52
Your Name <siddhpura306@gmail.com>
2023-06-19 12:39:15 +0530

    Edit url 3

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userCtrl = require("../controllers/userControl");
 // require("dotenv").config();
 // const baseUrl = "https://mern-crud-nn6k.onrender.com"
-router.get(`/https://mern-crud-nn6k.onrender.com/get`, userCtrl.getUser);
-router.post(`/https://mern-crud-nn6k.onrender.com/save`, userCtrl.saveUser);
-router.put(`/https://mern-crud-nn6k.onrender.com/update/:id`, userCtrl.updateUser);
-router.delete(`/https://mern-crud-nn6k.onrender.com/delete/:id`, userCtrl.deleteUser);
+router.get(`/get`, userCtrl.getUser);
+router.post(`/save`, userCtrl.saveUser);
+router.put(`/update/:id`, userCtrl.updateUser);
+router.delete(`/delete/:id`, userCtrl.deleteUser);
 
 module.exports = router;
